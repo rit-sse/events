6be52f9365731902938924a95ea92465da5f80c8
Michael Timbrook <timbrook480@gmail.com>
2016-03-28 17:40:04 -0400

    Add fetch for clientID

--- app/js/components/log-in.jsx
+++ app/js/components/log-in.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SSEStore from '../stores/sse';
+import API from '../api';
 
 export default class LogIn extends React.Component {
   constructor() {
@@ -7,12 +8,19 @@ export default class LogIn extends React.Component {
   }
 
   componentDidMount() {
-    // Retrieve the singleton for the GoogleAuth library and set up the client.
-    const auth2 = gapi.auth2.init({
-      client_id: process.env.GOOGLE_CLIENT_ID, // eslint-disable-line camelcase
-      cookie_policy: 'single_host_origin', // eslint-disable-line camelcase
-    });
-    auth2.attachClickHandler(this.refs.button.getDOMNode(), {}, SSEStore.signIn );
+    API
+      .Auth
+      .clientId()
+      .then( ({ token }) => {
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        const auth2 = gapi.auth2.init({
+          client_id: token, // eslint-disable-line camelcase
+          cookie_policy: 'single_host_origin', // eslint-disable-line camelcase
+        });
+        auth2.attachClickHandler(this.refs.button.getDOMNode(), {}, SSEStore.signIn );
+        this.setState({ disable_login: false });
+      })
+      .catch( err => console.log(err) );
   }
 
   render() {
