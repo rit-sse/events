673058b74782d1ee9fdb56b90d0a893b9f8e8247
Dylan Vuz <vuzdylan@gmail.com>
2016-04-26 15:00:06 -0400

    updated gulp file

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ import path from 'path';
 
 const app = express();
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server listening on port ' + server.address().port);
-});
-
 app.use('/events', express.static('dist'));
 
-app.use('/events/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+app.use('/events/*', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
+
+export default app;
